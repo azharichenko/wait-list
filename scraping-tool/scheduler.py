cb2a922c55cf5545457f752298fef028a2af6a1d
Alex Zharichenko <azharichenko@gmail.com>
2019-03-26 18:12:27 -0400

    Updated scheduler tool

--- scraping-tool/scheduler.py
+++ scraping-tool/scheduler.py
@@ -3,9 +3,9 @@ from subprocess import Popen, PIPE
 s = sched.scheduler(time.time, time.sleep)
 
 def run_downloader():
-    process = Popen(['./run_downloader'], stdout=PIPE, stderr=PIPE)
+    process = Popen(['bash', '/home/azharichenko/Projects/wait-list/run_downloader'], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     s.enter(15 * 60, 1, run_downloader)
 
-s.enter(15 * 60, 1, run_downloader)
+s.enter(5 * 60, 1, run_downloader)
 s.run()
