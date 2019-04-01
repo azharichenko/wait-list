8d83133af789868c7a275e9bc4ee0b073bf1874f
Alex D. Zharichenko <azharichenko@gmail.com>
2019-04-01 12:27:49 -0400

    Clean up tools

--- tools/scheduler.py
+++ tools/scheduler.py
@@ -7,5 +7,11 @@ def run_downloader():
     stdout, stderr = process.communicate()
     s.enter(15 * 60, 1, run_downloader)
 
+def run_section_number_scraper():
+    process = Popen(['pipenv', 'run', 'python', '/home/azharichenko/Projects/wait-list/tools/fetch-section-numbers'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    s.enter(24 * 60 * 60, 1, run_section_number_scraper)
+
 s.enter(5 * 60, 1, run_downloader)
+s.enter(0, 1, run_section_number_scraper)
 s.run()
