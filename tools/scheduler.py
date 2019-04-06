b40d3616e6676faa7d174c128808c3ab07b55e9c
Alex D. Zharichenko <azharichenko@gmail.com>
2019-04-05 23:47:43 -0400

    Adjusting to every hour download cycle

--- tools/scheduler.py
+++ tools/scheduler.py
@@ -5,7 +5,7 @@ s = sched.scheduler(time.time, time.sleep)
 def run_downloader():
     process = Popen(['bash', '/home/azharichenko/Projects/wait-list/run_downloader'], stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
-    s.enter(15 * 60, 1, run_downloader)
+    s.enter(60 * 60, 1, run_downloader)
 
 def run_section_number_scraper():
     process = Popen(['pipenv', 'run', 'python', '/home/azharichenko/Projects/wait-list/tools/fetch-section-numbers'], stdout=PIPE, stderr=PIPE)
